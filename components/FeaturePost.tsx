2a245a219428238350b49971aa7cdbb2e21774d7
Abdullahi254 <abdush504@gmail.com>
2022-12-29 13:14:41 +0300

    added Nextjs Link to home page carousel directing to actual post

--- components/FeaturePost.tsx
+++ components/FeaturePost.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image'
 import React from 'react'
-import { FeaturedPost} from '../typing'
+import { FeaturedPost } from '../typing'
 import moment from 'moment'
+import Link from 'next/link'
 
 type Props = {
     post: FeaturedPost
@@ -9,42 +10,42 @@ type Props = {
 
 const FeaturePost = ({ post }: Props) => {
     return (
-        <div className='relative group cursor-pointer'>
-            <Image
-                src={post.featuredImage.url.toString()}
-                alt={post.title.toString()}
-                width={500}
-                height={500}
-                priority
-                className=" h-[200px] w-[200px] xl:h-[300px] xl:w-[320px] opacity-40 group-hover:opacity-70"
-            />
-
-            <span className='top-1 xl:top-3 absolute z-10 w-[200px] xl:w-[320px] flex justify-center items-center text-white'>
-                {moment(post.updatedAt).format('DD MMM, YYYY')}
-            </span>
-
-            <div className='absolute z-10 top-[20%] w-[200px] xl:w-[320px] px-2'>
-                <p className=' text-white font-semibold text-center text-lg'>
-                    {post.title}
-                </p>
-            </div>
-
-            <div className=' bottom-1 xl:bottom-3 absolute z-10 w-[200px] xl:w-[320px] flex justify-center items-center'>
+        <Link href={`/posts/${post.slug}`}>
+            <div className='relative group cursor-pointer'>
                 <Image
+                    src={post.featuredImage.url.toString()}
+                    alt={post.title.toString()}
+                    width={500}
+                    height={500}
                     priority
-                    src={post.author.photo.url.toString()}
-                    alt={post.author.name.toString()}
-                    width={50}
-                    height={50}
-                    className="w-[30px] h-[30px] rounded-full mr-2"
+                    className=" h-[200px] w-[200px] xl:h-[300px] xl:w-[320px] opacity-40 group-hover:opacity-70"
                 />
-                <span className=' text-sm text-white'>
-                    {post.author.name}
+
+                <span className='top-1 xl:top-3 absolute z-10 w-[200px] xl:w-[320px] flex justify-center items-center text-white'>
+                    {moment(post.updatedAt).format('DD MMM, YYYY')}
                 </span>
+
+                <div className='absolute z-10 top-[20%] w-[200px] xl:w-[320px] px-2'>
+                    <p className=' text-white font-semibold text-center text-lg'>
+                        {post.title}
+                    </p>
+                </div>
+
+                <div className=' bottom-1 xl:bottom-3 absolute z-10 w-[200px] xl:w-[320px] flex justify-center items-center'>
+                    <Image
+                        priority
+                        src={post.author.photo.url.toString()}
+                        alt={post.author.name.toString()}
+                        width={50}
+                        height={50}
+                        className="w-[30px] h-[30px] rounded-full mr-2"
+                    />
+                    <span className=' text-sm text-white'>
+                        {post.author.name}
+                    </span>
+                </div>
             </div>
-
-
-        </div>
+        </Link>
     )
 }
 
